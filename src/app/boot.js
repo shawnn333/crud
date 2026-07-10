3328f1e08e210627eb436c16d05c94d30562c072
shawnn333 <shawnmartinez316@gmail.com>
2026-07-10 08:59:02 +0800

    Update boot.js

--- src/app/boot.js
+++ src/app/boot.js
@@ -5,15 +5,12 @@ import { GetTaskUseCase } from '../domain/usecases/getTaskUseCase.js';
 import { RemoveTaskUseCase } from '../domain/usecases/removeTaskUseCase.js';
 import { UpdateTaskUseCase } from '../domain/usecases/updateTaskUseCase.js';
 
-// Create repository instance
 const taskRepository = new LocalStorageTaskRepository();
 
-// Create and export use cases
 export const addTaskUseCase = new AddTaskUseCase(taskRepository);
 export const getAllTasksUseCase = new GetAllTasksUseCase(taskRepository);
 export const getTaskUseCase = new GetTaskUseCase(taskRepository);
 export const removeTaskUseCase = new RemoveTaskUseCase(taskRepository);
 export const updateTaskUseCase = new UpdateTaskUseCase(taskRepository);
 
-// Export repository for direct access if needed
 export { taskRepository };

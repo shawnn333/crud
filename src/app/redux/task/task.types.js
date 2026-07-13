705c33d8eacc85c9ebe5b47bf9fe011e7bef01ac
shawn <shawnmartinez316@gmail.com>
2026-07-13 13:29:05 +0800

    restore accidentally deleted files

---
+++ src/app/redux/task/task.types.js
@@ -0,0 +1,43 @@
+export const ADD_TASK = 'tasks/addTask';
+export const DELETE_TASK = 'tasks/deleteTask';
+export const TOGGLE_TASK = 'tasks/toggleTask';
+export const EDIT_TASK = 'tasks/editTask';
+export const SET_FILTER = 'tasks/setFilter';
+export const SET_ACTIVE_NAV = 'tasks/setActiveNav';
+export const SET_LOADING = 'tasks/setLoading';
+export const SET_ERROR = 'tasks/setError';
+export const CLEAR_TASKS = 'tasks/clearTasks';
+export const SET_TASKS = 'tasks/setTasks';
+
+
+export const FETCH_TASKS_REQUEST = 'tasks/fetchTasksRequest';
+export const FETCH_TASKS_SUCCESS = 'tasks/fetchTasksSuccess';
+export const FETCH_TASKS_FAILURE = 'tasks/fetchTasksFailure';
+
+export const ADD_TASK_REQUEST = 'tasks/addTaskRequest';
+export const ADD_TASK_SUCCESS = 'tasks/addTaskSuccess';
+export const ADD_TASK_FAILURE = 'tasks/addTaskFailure';
+
+export const DELETE_TASK_REQUEST = 'tasks/deleteTaskRequest';
+export const DELETE_TASK_SUCCESS = 'tasks/deleteTaskSuccess';
+export const DELETE_TASK_FAILURE = 'tasks/deleteTaskFailure';
+
+export const TOGGLE_TASK_REQUEST = 'tasks/toggleTaskRequest';
+export const TOGGLE_TASK_SUCCESS = 'tasks/toggleTaskSuccess';
+export const TOGGLE_TASK_FAILURE = 'tasks/toggleTaskFailure';
+
+export const EDIT_TASK_REQUEST = 'tasks/editTaskRequest';
+export const EDIT_TASK_SUCCESS = 'tasks/editTaskSuccess';
+export const EDIT_TASK_FAILURE = 'tasks/editTaskFailure';
+
+
+export const NAV_ALL = 'all';
+export const NAV_TODAY = 'today';
+export const NAV_UPCOMING = 'upcoming';
+export const NAV_THIS_WEEK = 'this-week';
+export const NAV_THIS_MONTH = 'this-month';
+
+
+export const FILTER_ALL = 'all';
+export const FILTER_COMPLETED = 'completed';
+export const FILTER_PENDING = 'pending';

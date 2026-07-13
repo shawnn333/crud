44986466b8c81171cb31f72f21c2bcf23cb657aa
shawnn333 <shawnmartinez316@gmail.com>
2026-07-13 11:16:29 +0800

    Update taskRepositoryProvider.js

--- src/app/taskRepositoryProvider.js
+++ src/app/taskRepositoryProvider.js
@@ -1,44 +1,24 @@
 import { LocalStorageTaskRepository } from '../data/repositories/LocalStorageTaskRepository.js';
 import { InMemoryTaskRepository } from '../data/repositories/InMemoryTaskRepository.js';
 
-// Repository types
 export const RepositoryType = {
   LOCAL_STORAGE: 'localStorage',
   IN_MEMORY: 'inMemory',
 };
 
-// Singleton instance
 let repositoryInstance = null;
 let repositoryType = RepositoryType.LOCAL_STORAGE;
 
-/**
- * Get the task repository instance
- * @param {string} type - Repository type (localStorage or inMemory)
- * @returns {ITaskRepository} Repository instance
- */
-export const getTaskRepository = (type = repositoryType) => {
-  if (!repositoryInstance || type !== repositoryType) {
-    repositoryType = type;
-    repositoryInstance = createRepository(type);
-  }
-  return repositoryInstance;
-};
-
-/**
- * Create a repository instance
- * @param {string} type - Repository type
- * @returns {ITaskRepository} Repository instance
- */
 const createRepository = (type) => {
   switch (type) {
     case RepositoryType.LOCAL_STORAGE:
       const localStorageRepo = new LocalStorageTaskRepository();
-      // Add sample data if empty
+
       localStorageRepo.addSampleDataIfEmpty();
       return localStorageRepo;
     case RepositoryType.IN_MEMORY:
       const inMemoryRepo = new InMemoryTaskRepository();
-      // Add sample data for demonstration
+
       inMemoryRepo.addSampleData();
       return inMemoryRepo;
     default:
@@ -46,21 +26,14 @@ const createRepository = (type) => {
   }
 };
 
-/**
- * Clear and reset the repository
- * @param {string} type - Repository type
- */
+
 export const resetRepository = (type = repositoryType) => {
   repositoryInstance = null;
   repositoryType = type;
   return getTaskRepository(type);
 };
 
-/**
- * Get all use cases
- * @param {ITaskRepository} repository - Repository instance
- * @returns {Object} Use cases
- */
+
 export const getUseCases = (repository = getTaskRepository()) => {
   const { AddTaskUseCase } = require('../domain/usecases/addTaskUseCase');
   const { GetAllTasksUseCase } = require('../domain/usecases/getAllTasksUseCase');
@@ -77,5 +50,5 @@ export const getUseCases = (repository = getTaskRepository()) => {
   };
 };
 
-// Export a default instance with localStorage
+
 export default getTaskRepository();
